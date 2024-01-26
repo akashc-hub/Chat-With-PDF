9c8857024eb2b2810a84830ceda0db302ae9b3b0
Akash <akashchauhan84590@gmail.com>
2024-01-26 21:02:57 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,6 +63,27 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {/* feature section */}
+      <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
+        <div className="mb-12 px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl sm:text-center">
+            <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">Start chatting in minutes
+            </h2>
+            <p className="mt-4 text-lg text-gray-600">
+            </p>
+          </div>
+        </div>
+
+        {/* Steps */}
+        <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+          <li className="md:flex-1">
+            <div className="flex flex-col">
+
+            </div>
+          </li>
+        </ol>
+      </div>
       </>
   );
 }
