04edc1ac5424ddd1618507c64fc26492d0367579
Akash <akashchauhan84590@gmail.com>
2024-01-26 13:27:27 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
   return (
       <div>
-        
+        <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center"></MaxWidthWrapper>
       </div>
   );
 }
