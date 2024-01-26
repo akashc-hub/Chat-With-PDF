8c0671a5cad96a158f2a1b81e3cbdbec4549eca6
Akash <akashchauhan84590@gmail.com>
2024-01-26 20:34:39 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
           <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur translate-all hover:border-gray-300 hover:bg-white/50">
             <p className="text-sm font-semibold text-gray-700">
-              Chat With PDF
+              Chat with PDF
             </p>
           </div>
           <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
@@ -47,11 +47,19 @@ export default function Home() {
                     src="/dashboard-preview.jpg" 
                     alt="product preview"
                     width={1364}
-                    height={866}/>
+                    height={866}
+                    quality={100}
+                    className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl right-1 ring-gray-900/10"
+                    />
                 </div>
-
               </div>
             </div>
+          </div>
+
+          <div aria-hidden="true" className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gnu overflow-hidden blur-3xl sm:-top-80">
+            <div style={{
+              clipPath : "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+            }} className="relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]" />
           </div>
         </div>
       </div>
