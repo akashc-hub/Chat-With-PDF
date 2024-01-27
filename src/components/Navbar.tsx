25bfe652745108bf6738034fd6a21f68dc2c5963
Akash <akashchauhan84590@gmail.com>
2024-01-27 13:16:05 +0530

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,45 @@
-import { Link } from "lucide-react"
+import { ArrowRight, Link } from "lucide-react"
 import MaxWidthWrapper from "./MaxWidthWrapper"
+import { buttonVariants } from "./ui/button"
+import dynamic from "next/dynamic"
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server"
 
 const Navbar = () => {
   return (
       <nav className="sticky h-14 inset-x-0 top-0 z-30 border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
         <MaxWidthWrapper>
             <div className="flex h-14 items-center justify-between border-b border-zinc-200">
-                <Link href="/" className="flex z-40 font-semibold">
-                    <span>FileTalk..</span>
+              <>
+                <Link 
+                  href="/" 
+                  className="flex z-40 font-semibold">
+                    <span>FileTalk...</span>
                 </Link>
+                </>
+                {/* Todo : add mobile navbar */}
+                <div className="hidden items-center space-x-4 sm:flex">
+                  <>
+                    <Link href="/pricing" className={buttonVariants({
+                      variant: "ghost",
+                      size: "sm",
+                    })}>Pricing</Link>
+                    <LoginLink className={buttonVariants({
+                      variant: "ghost",
+                      size: "sm",
+                    })}>
+                      Sign UP
+                    </LoginLink>
+                    <RegisterLink className={buttonVariants({
+                      size: "sm",
+                    })}>
+                      Get Started <ArrowRight className="ml-1.5 h-5 w-5"/>
+                    </RegisterLink>
+                  </>
+                </div>
             </div>
         </MaxWidthWrapper>
       </nav>
   )
 }
 
-export default Navbar
+export default dynamic (() => Promise.resolve(Navbar), {ssr: false})
