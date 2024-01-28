682527108f1486e6ff8211ae6e885ef9fb30bce0
Akash <akashchauhan84590@gmail.com>
2024-01-28 13:06:02 +0530

    first commit

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import Dashboard from "@/components/Dashboard";
+import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -5,12 +7,18 @@ const Page = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser(); 
 
-  if (!user || !user.id) {
+  if (!user || !user.id)
     redirect('/auth-callback?origin=dashboard');
-    return null; 
-  }
+    
+    const dbUser = await db.user.findFirst({
+      where : {
+        id: user.id
+      }
+    })
+    
+    if(!dbUser) redirect('/auth-callback?origin=dashboard')
 
-  return <div>{user.email}</div>;
+  return <Dashboard/>
 };
 
 export default Page;
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,10 @@
+
+const Dashboard = () => {
+  return (
+    <div>
+      Hello World
+    </div>
+  )
+}
+
+export default Dashboard
